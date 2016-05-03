a1edce7b2f5033e5f78d2d5cc858a5eef21e105f
LAC Louis <llac@ravenelle.pedago.ipb.fr>
2016-05-03 11:08:49 +0200

    debut dictionnaire

---
+++ Dictionary.c
@@ -0,0 +1,30 @@
+struct combinaison 
+{
+  int nb_pion = 4; //par defaut 
+  int nb_couleur = 8;
+  int combi[nb_pion];
+};
+
+int nb_combi_dict(FILE * dict)
+{
+  char line[100];
+  int nb_line = 0;
+  while(!feof(dict))
+    {
+      fgets(line, 100, file);
+      if( )
+	nb_line ++;
+      else
+	}
+  return nb_line - 1; // on ne compte pas le titre
+}
+
+struct combinaison get_combi_dict(FILE * dict)
+{
+  char line[100];
+  int line_count =0;
+  while(!(feof(dict))
+    {
+      
+    }
+    }
