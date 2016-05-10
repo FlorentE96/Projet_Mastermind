60880e55b9157fb6e431fa47b9dd5355e7fccfb2
LAC Louis <llac@pavot.pedago.ipb.fr>
2016-05-10 11:08:05 +0200

    changements louis

---
+++ Force_brute.c
@@ -0,0 +1,26 @@
+//trouve la combi en comparant 1 par 1 les pions placés
+
+#DEFINE NB_PION (4)
+
+void Permutation(*int tab1[NB_PION], int i, int j)
+{
+  int temp = *tab[i];
+  *tab[i] = *tab[j];
+  *tab[j] = temp;
+}
+
+
+void  Algo_1(struct * joueur Ordi, int tab[NB_PION])
+{
+  //initialisation du tab à 0 (pion de couleur 0)
+  for(int i =0, i < NB_PION, i++) tab[i] = 0; 
+  int pion = 0;
+  comparaison(Ordi->combi_a_trouver, tab)
+  int couleur_bien_place = Ordi->
+  //on parcours les pion qui vont êtres comparés à la combinaison
+ 
+ while(pion < NB_PION)
+    {
+      comparaison(Ordi->combi_a_trouver,tab) 
+    }
+}
