7294752965a8509b94db62ac6d53ee9ffcbabfb5
Lac Louis <laclouis5@MacBook-Air-de-Louis.local>
2016-05-20 14:36:06 +0200

    elaboration des headers

--- fonctions.h
+++ fonctions.h
@@ -1,100 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef FONCTION_HEADER
+#define FONCTION_HEADER
 
 #define NB_PIONS (4)
 #define NB_COULEURS (8)
 #define MAX_COMBI (10000)
 #define MAX_COUPS (100)
-#definine MAX_NAME (30)
+#define MAX_NAME (30)
 
-struct Joueur
-{ char *nom;
-  int nb_pts;
-  int nb_coups;
-  int combi_a_trouver[NB_PIONS];
-  int combinaisons_passees[MAX_COUPS][NB_PIONS];
-  int resultats_passes[MAX_COUPS][2];
-};
+struct Joueur;
 
-void init_tab(int * tab, int valeur, int nb_case)
-{
-    for(int i = 0; i < nb_case; i++)
-        tab[i] = valeur;
-}
+void init_tab(int * tab, int valeur, int nb_case);
 
-void init_combi_val(int *tab, const int *ref, int from, int val)
-{
-    int i = from;
-    while(i < NB_PIONS)
-    {
-        if(ref[i] == -1)
-            i ++;
-        else
-        {
-            tab[i] = val;
-            i ++;
-        }
-    }
-}
+void init_combi_val(int *tab, const int *ref, int from, int val);
 
-void copy_tab(int * source, int * destination, int taille)
-{
-    for(int i = 0; i < taille; i ++)
-    {
-        destination[i] = source[i];
-    }
-}
+void copy_tab(int * source, int * destination, int taille);
 
-void print_array_n (int *tableau, int taille)
-{
-    for (int i =0; i<taille; i++)
-    {
-        printf("%d ", tableau[i]);
-    }
-    printf("\n");
-}
+void print_array_n (int *tableau, int taille);
 
-void comparaison(struct Joueur *joueur, int proposition[])
-{
-    joueur->nb_coups ++;
-    int combi[NB_PIONS];
-    copy_tab(joueur->combi_a_trouver, combi, NB_PIONS);
-    
-    for (int i = 0; i < NB_PIONS; i ++)
-    {
-        if(combi[i] == proposition[i])
-        {
-            combi[i] = -1;
-            joueur->resultats_passes[joueur->nb_coups][0] ++;
-        }
-    }
-    
-    for (int i = 0; i < NB_PIONS; i ++)
-    {
-        for (int j = 0; j < NB_PIONS; j ++)
-        {
-            if(j != i && combi[j] != -1 && combi[j] == proposition[i])
-            {
-                joueur->resultats_passes[joueur->nb_coups][1] ++;
-                combi[j] = -1;
-                break;
-            }
-        }
-    }
-}
+void comparaison(struct Joueur *joueur, int proposition[]);
 
-void init_player_keep_score(struct Joueur *joueur)
-{
-    joueur->nb_coups = 0;
-    
-    for(int m = 0; m < MAX_COUPS; m++)
-    {
-        joueur->resultats_passes[m][0] = 0;
-        joueur->resultats_passes[m][1] = 0;
-        joueur->combinaisons_passees[m][0] = 0;
-        joueur->combinaisons_passees[m][1] = 0;
-        joueur->combinaisons_passees[m][2] = 0;
-        joueur->combinaisons_passees[m][3] = 0;
-    }
-}
+void init_player_keep_score(struct Joueur *joueur);
 
+#endif
