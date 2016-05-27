cc4d56eac503456ffaf304c7d5e82f8de44e3c63
FlorentE <florent@koma-elektronik.com>
2016-05-27 16:53:38 +0200

    correction erreur points virgules

--- gestion_jeu.h
+++ gestion_jeu.h
@@ -2,9 +2,9 @@
 #define GESTION_JEU_H
 
 void saisie_combi(int * combi);
-void afficher_jeu(struct Joueur * joueur)
-int multijoueur(struct Joueur * joueurs) // retourne le numéro du joueur gagnant (joueur 0 ou joueur 1)
-int monojoueur(struct Joueur * joueur) // retourne 1 si gagné, 0 si perdu
+void afficher_jeu(struct Joueur * joueur);
+int multijoueur(struct Joueur * joueurs); // retourne le numéro du joueur gagnant (joueur 0 ou joueur 1)
+int monojoueur(struct Joueur * joueur); // retourne 1 si gagné, 0 si perdu
 
 
 #endif
